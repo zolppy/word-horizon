0354f0ffdddca6b540bbec9d4c60bfd392edc290
Gabriel Cavalcante <gabriel.lcifba@gmail.com>
2025-07-06 11:44:58 -0300

    fix: licença de uso e links do footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,22 +1,32 @@
-import { FaGithub } from "react-icons/fa";
+import { FaCreativeCommons, FaGithub } from "react-icons/fa6";
 
 export function Footer() {
   return (
     <footer className="text-center py-4 bg-black text-white p-2 flex justify-center">
       <div className="flex flex-col items-center justify-center w-fit lg:flex-row lg:gap-15 lg:ml-[300px]">
-        <p>
-          2025 &#127279; <b>Word Horizon</b>
+        <p className="flex gap-x-2 items-center">
+          2025 <FaCreativeCommons />
+          <b>Word Horizon</b>
         </p>
         <div className="flex flex-row gap-2 items-center">
           <i className="text-2xl">
             <FaGithub />
           </i>
           <div className="flex flex-row gap-1">
-            <a href="https://github.com/MegMinnie/">
-              {" "}
-              Desenvolvido por Mirele Oliveira
+            Desenvolvido por
+            <a
+              href="https://github.com/MegMinnie/"
+              className="text-purple-700 lg:hover:underline lg:hover:cursor-pointer"
+            >
+              Mirele Oliveira
             </a>
-            <a href="https://github.com/zolppy">e Gabriel Oliveira</a>
+            e
+            <a
+              href="https://github.com/zolppy"
+              className="text-purple-700 lg:hover:underline lg:hover:cursor-pointer"
+            >
+              Gabriel Oliveira
+            </a>
           </div>
         </div>
       </div>
