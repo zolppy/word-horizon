713b5525b1cd3bc4f9bd0a48abdf6d521435034e
Megminnie <mirelee3456@gmail.com>
2025-07-09 06:55:47 +0000

    fix: Foi criado a estrutura dos nomes das categorias

---
+++ src/utils/data/ emotions.ts
@@ -0,0 +1,15 @@
+import { v4 as uuid } from "uuid";
+import type { WordCard as WordCardType } from "../types/WordCard";
+import animals from "../../assets/images/categories/animals.jpg";
+
+const Emotions: WordCardType[] = [
+  {
+    id: uuid(),
+    nome: "cat",
+    img: animals,
+    som: "som1",
+    traducao: "gato",
+  },
+];
+
+export { Emotions };
